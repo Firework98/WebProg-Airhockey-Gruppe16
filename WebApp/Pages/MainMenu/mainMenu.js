04e80aec6a382b0ce067572a85565452e4097f35
JRunnerL <jonas.laufer@t-online.de>
2018-10-23 10:58:56 +0200

    Mute Button zusammen gelegt mit Unmute

--- WebApp/Pages/MainMenu/mainMenu.js
+++ WebApp/Pages/MainMenu/mainMenu.js
@@ -1,11 +1,9 @@
 function audioStop() {
     var sound = document.getElementById("audio");
+
+    if (sound.paused != false) {
+        sound.play();
+    } else {
         sound.pause();
-        console.log(sound.played);
-        sound.played = 0;
-}
-function audioStart() {
-    var sound = document.getElementById("audio");
-    sound.play();
-    console.log(sound.played);
+    }
 }
