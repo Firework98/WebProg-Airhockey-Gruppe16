5485f29b9dfaadeb086681ab3c5fee6cda2a062a
Firework98 <to.sengpiel@gmail.com>
2018-11-06 10:59:46 +0100

    Added Game Logic Added Collision Added Disk

--- WebApp/Pages/Game/game.js
+++ WebApp/Pages/Game/game.js
@@ -3,15 +3,108 @@ let gC;
 let pX;
 let pY;
 let pPush;
+let gDsk;
 
+const DECAY = 0.999;
+const REDUCTION = 0.95;
 const WIDTH = 640;
 const HEIGHT = 480;
+const StackSize = 3;
 
+class Vec2D{
+    constructor (x,y){
+        this.x = x;
+        this.y = y;
+    }
+    multiply (scalar){
+        this.x *= scalar;
+        this.y *= scalar;
+    }
+    length(){
+        return Math.sqrt(this.x * this.x + this.y * this.y)
+    }
+    normalize(){
+        let temp = this.length();
+        this.x /= temp;
+        this.y /= temp;
+    }
+    clone(){
+        return (new Vec2D(this.x,this.y));
+    }
+}
+class Disk{
+
+    constructor (radius,x,y){
+        this.radius = radius;
+        this.x = x;
+        this.y = y;
+        this.velo = new Vec2D(5,5);
+        this.col = "red";
+    }
+    render(){
+        gC.fillStyle = this.col;
+        gC.beginPath();
+
+        gC.arc(this.x,this.y,this.radius,0, 2*Math.PI);
+        gC.fill();
+    }
+    move(){
+        this.x += this.velo.x;
+        this.y += this.velo.y;
+        this.velo.x *= DECAY;
+        this.velo.y *= DECAY;
+        this.checkCollisionWithBorder();
+        this.checkCollisionWithPusher(pPush);
+    }
+    checkCollisionWithBorder(){
+        if (this.x + this.radius > WIDTH){
+            this.x = 640 - this.radius;
+            this.velo.x = -(this.velo.x * REDUCTION)
+        }
+        if (this.x - this.radius < 0){
+            this.x = this.radius;
+            this.velo.x = -(this.velo.x * REDUCTION)
+        }
+        if (this.y + this.radius > HEIGHT){
+            this.y = HEIGHT - this.radius;
+            this.velo.y = -(this.velo.y * REDUCTION)
+        }
+        if (this.y - this.radius < 0){
+            this.y = 0 + this.radius;
+            this.velo.y = -(this.velo.y * REDUCTION)
+        }
+        console.log("Velo =" + this.velo);
+    }
+    checkCollisionWithPusher(pusher){
+        let distVec = new Vec2D(this.x-pusher.x,this.y-pusher.y);
+        console.log("Dist"+distVec.x + " | " + distVec.y);
+        if (distVec.length() < pusher.radius + this.radius){
+            this.col = "blue";
+            let distDir = distVec.clone();
+            distDir.normalize();
+            let pOldVec = pusher.getLast();
+            let pVelo = new Vec2D(pusher.x - pOldVec.x, pusher.y - pOldVec.y);
+            console.error("Pvelo = ("+ pVelo.x + " | " + pVelo.y + ")");
+            distDir.multiply(Math.sqrt(pVelo.length()) + this.velo.length());
+            this.velo = distDir;
+        } else{
+            this.col = "red";
+        }
+    }
+}
 class Pusher{
     constructor (radius,x,y){
         this.radius = radius;
         this.x = x;
         this.y = y;
+        this.stack = [];
+        for(var i = 0; i < StackSize; i++){
+            this.stack.push(new Vec2D(x,y));
+        }
+    }
+    getLast(){
+        return this.stack[0];
+        
     }
 
     render(){
@@ -23,8 +116,30 @@ class Pusher{
     }
 
     moveTo(x,y){
+        let newPos = this.checkBorderCollision(x,y);
+        this.setPos(newPos.x, newPos.y);
+    }
+    setPos(x,y){
+        this.stack.push(new Vec2D (x,y));
+        this.stack.shift();
         this.x = x;
         this.y = y;
+    }
+
+    checkBorderCollision(newX,newY){
+        if (newX + this.radius > WIDTH){
+            newX = 640 - this.radius;
+        }
+        if (newX - this.radius < 0){
+            newX = this.radius;
+        }
+        if (newY + this.radius > HEIGHT){
+            newY = HEIGHT - this.radius;
+        }
+        if (newY - this.radius < 0){
+             newY = 0 + this.radius;
+        }
+        return new Vec2D(newX,newY);
     }
 }
 function init(){
@@ -32,15 +147,15 @@ function init(){
     if(canv != null){
         gC = canv.getContext("2d");
     }
-
-    let psh = new Pusher(10,50,40);
+    let psh = new Pusher(40,50,40);
+    let dsk = new Disk(20,40,50);
     pPush = psh;
+    gDsk = dsk;
     window.requestAnimationFrame(draw);
     document.addEventListener("mousemove",function (e){
         let x = e.pageX;
         let y = e.pageY;
-        pX = x;
-        pY = y;
+        pPush.moveTo(x,y);
         console.log("x " + x + "y " + y);
     })
 }
@@ -49,19 +164,8 @@ function draw(){
     gC.fillStyle = "grey";
     gC.fillRect(0,0,640,480);
     gC.fill();
-    if (pX + pPush.radius > WIDTH){
-        pX = 640 - pPush.radius;
-    }
-    if (pX - pPush.radius < 0){
-        pX = pPush.radius;
-    }
-    if (pY + pPush.radius > HEIGHT){
-        pY = HEIGHT - pPush.radius;
-    }
-    if (pY - pPush.radius < 0){
-        pY = 0 + pPush.radius;
-    }
-    pPush.moveTo(pX, pY);
+    gDsk.move();
+    gDsk.render();
     pPush.render();
     window.requestAnimationFrame(draw);
 }
