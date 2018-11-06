b99192c3a74eb7da370419d783b02fba766a8572
JRunnerL <jonas.laufer@t-online.de>
2018-11-06 19:42:18 +0100

    Highscore List geht!

--- WebApp/Pages/Highscore/highscore.js
+++ WebApp/Pages/Highscore/highscore.js
@@ -3,60 +3,60 @@ window.onload = init;
 function init() {
     const button = document.getElementById('save');
     button.onclick = addEntry;
-    const eintraegeArray = getLocalHighscore();
-    for (let i = 0; i < eintraegeArray.length; i++) {
-        writeIntoDOM(i, eintraegeArray[i]);
+    const localHighscoreArr = getLocalHighscore();
+    for (let i = 0; i < localHighscoreArr.length; i++) {
+        writeIntoDOM(i, localHighscoreArr[i]);
     }
 }
 
 function getLocalHighscore() {
-    let eintraegeArray = localStorage.getItem('eintraegeArray');
-    eintraegeArray = eintraegeArray ? JSON.parse(eintraegeArray) : [];
-    return eintraegeArray;
+    let localHighscoreArr = localStorage.getItem('localHighscore');
+    localHighscoreArr = localHighscoreArr ? JSON.parse(localHighscoreArr) : [];
+    return localHighscoreArr;
 }
 
 function addEntry() {
-    let eintraegeArray = getLocalHighscore();
-    const value = document.getElementById('eingabe').value;
-    const punktzahl = document.getElementById('punktzahl').value;
-    if (value !== '' && punktzahl !== '') {
-        const eintrag = {'Punktzahl': punktzahl, 'Name': value};
+    let localHighscoreArr = getLocalHighscore();
+    const username = document.getElementById('inputName').value;
+    const score = document.getElementById('inputScore').value;
+    if (username !== '' && score !== '') {
+        const newEntry = {'Score': score, 'Username': username};
         let i;
-        if (eintraegeArray.length !== 0) {
-            for (i = 0; i < eintraegeArray.length; i++) {
-                if (parseInt(punktzahl) >= parseInt(eintraegeArray[i].Punktzahl)) {
-                    eintraegeArray.splice(i, 0, eintrag);
+        if (localHighscoreArr.length !== 0) {
+            for (i = 0; i < localHighscoreArr.length; i++) {
+                if (parseInt(score) >= parseInt(localHighscoreArr[i].Score)) {
+                    localHighscoreArr.splice(i, 0, newEntry);
                     break;
-                } else if ((i) === eintraegeArray.length-1) {
-                    console.log("Input");
-                    eintraegeArray.push(eintrag);
+                } else if ((i) === localHighscoreArr.length-1) {
+                    localHighscoreArr.push(newEntry);
+                    break;
                 }
             }
         } else {
-            eintraegeArray.push(eintrag);
+            localHighscoreArr.push(newEntry);
         }
-        localStorage.setItem('eintraegeArray', JSON.stringify(eintraegeArray));
-        writeIntoDOM(i, eintrag.Name);
+        localStorage.setItem('localHighscore', JSON.stringify(localHighscoreArr));
+        writeIntoDOM(i, newEntry);
     } else {
-        alert('Bitte geben Sie etwas ein!');
+        alert('Please insert!');
     }
 }
 
 function deleteScoreEntry(entry) {
     const id = entry.target.id;
-    let eintraegeArray = getLocalHighscore();
-    if (eintraegeArray) {
-        eintraegeArray.splice(id, 1);
+    let localHighscoreArr = getLocalHighscore();
+    if (localHighscoreArr) {
+        localHighscoreArr.splice(id, 1);
     }
-    localStorage.setItem('eintraegeArray', JSON.stringify(eintraegeArray));
+    localStorage.setItem('localHighscore', JSON.stringify(localHighscoreArr));
     deleteOutOfDOM(id);
 }
 
 function writeIntoDOM(id, ItemObj) {
-    let htmlEntries = document.getElementById('eintraege');
+    let htmlEntries = document.getElementById('entries');
     let newHtmlEntry = document.createElement('li');
     newHtmlEntry.setAttribute('id', id);
-    newHtmlEntry.innerHTML = "" + ItemObj.Name + " : " + ItemObj.Punktzahl;
+    newHtmlEntry.innerHTML = "" + ItemObj.Username + " : " + ItemObj.Score;
     htmlEntries.appendChild(newHtmlEntry);
     newHtmlEntry.onclick = deleteScoreEntry;
 }
